fd4151f3e16b099eee38ee4e1789139cce1975ab
AlbertoCarena <100611068+AlbertoCarena@users.noreply.github.com>
2022-03-16 14:22:58 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
-using namespace std;
 
-int main(){
-//Qui vanno definite le variabili
+int main()
+{
+  float a; float b; float c;
+  float Atriangolo=((a*b)/2); float Aquadrato=(a*a); float Arettangolo=(a*b); float Atrapezio=(((a+b)*c)/2);
+  
+  std::cin>>a>>b>>c;
+  std::cout<<((a*b)/2)<<std::endl;
+  std::cout<<(a*a)<<std::endl;
+  std::cout<<(a*b)<<std::endl;
+  std::cout<<(((a+b)*c)/2)<<std::endl;
 
-cin >> a >> b >> c;
-
-//Qui va scritto il codice per eseguire i calcoli richiesti
+return 0;
 
 
 }
